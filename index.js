24913d1f7a2a2c3f356e057507fa94a2d5789185
Japkozjad <japkozjad@gmail.com>
2025-05-04 20:39:53 +0200

    Added Write As System shorcut

--- index.js
+++ index.js
@@ -1,17 +1,22 @@
 // Add an event listener for keydown events
 document.addEventListener('keydown', (event) => {
-    // Check if the Alt key and the "I" key are pressed together
+    // Impersonate
     if (event.altKey && event.key.toLowerCase() === 'i') {
         event.preventDefault(); // Prevent default browser behavior
 
-        // Trigger the /impersonate slash command
-        const inputField = document.querySelector('#send_textarea'); // Adjust selector if needed
+        const inputField = document.querySelector('#send_textarea');
         if (inputField) {
             inputField.value = '/impersonate ';
-            const event = new KeyboardEvent('keydown', { key: 'Enter', bubbles: true });
-            inputField.dispatchEvent(event);
-        } else {
-            console.error('Input field not found!');
+        }
+    }
+
+    if (event.altKey && event.key.toLowerCase() === 's') {
+        event.preventDefault(); // Prevent default browser behavior
+
+        const systemButton = document.querySelector('.qr--button.menu_button.qr--hasCtx.interactable[title="Write As System"]');
+        if (systemButton) {
+            systemButton.click();
+            document.querySelector('#send_textarea').focus();
         }
     }
 });
